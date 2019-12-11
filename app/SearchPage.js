e58a99ac9825331a646d192cc40051bdef9df427
Trung Hieu <hieutt@d-soft.com.vn>
2019-12-11 17:52:37 +0700

    Init navigator

---
+++ app/SearchPage.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import {StyleSheet, Text, View, TextInput, Button, SafeAreaView} from 'react-native';
+
+export default class SearchPage extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.description}>Search for houses to buy!</Text>
+        <Text style={styles.description}>
+          Search by place-name or postcode.
+        </Text>
+        <View style={styles.flowRight}>
+          <TextInput
+            style={styles.searchInput}
+            placeholder="Search via name or postcode"
+          />
+          <Button onPress={() => {}} color="#48BBEC" title="Go" />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  description: {
+    marginBottom: 20,
+    fontSize: 18,
+    textAlign: 'center',
+    color: '#656565',
+  },
+  container: {
+    padding: 30,
+    marginTop: 65,
+    alignItems: 'center',
+  },
+  flowRight: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+  },
+  searchInput: {
+    height: 36,
+    padding: 4,
+    marginRight: 5,
+    flexGrow: 1,
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: '#48BBEC',
+    borderRadius: 8,
+    color: '#48BBEC',
+  },
+});
